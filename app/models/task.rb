c69be8095084b4d098e1f5e7b017ef5c69f34701
Yuji TANI <yuji.tani.jp@gmail.com>
2019-07-27 19:51:15 +0900

    タスクに画像を添付できるように実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,10 @@ class Task < ApplicationRecord
   validates :name, length: {maximum: 30}
   validate :validate_name_not_including_comma
 
-  belongs_to :user
-
   scope :record_entity_expansion, -> {order(created_at: :desc)}
+
+  belongs_to :user
+  has_one_attached :image
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name created_at]
