2f14f378524d364120cf5fa65bd9b607a7aa6679
Yuji TANI <yuji.tani.jp@gmail.com>
2019-07-28 00:28:19 +0900

    ajaxでタスクを削除するように実装

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -9,3 +9,14 @@ document.addEventListener('turbolinks:load', function () {
     });
   });
 });
+
+document.addEventListener('turbolinks:load', function () {
+  document.querySelectorAll('.delete').forEach(function (a) {
+    a.addEventListener('ajax:success', function () {
+      let td = a.parentNode;
+      let tr = td.parentNode;
+      tr.style.display = 'none';
+    });
+  });
+});
+
